34a84e59bd61c8d43fca81a690c71ce25ae7da79
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2021-08-30 14:54:01 -0300

    feat(ParkingLot): calculate beach, shopping and airport

--- 2-solid-parking-lot/src/ParkingLot.spec.ts
+++ 2-solid-parking-lot/src/ParkingLot.spec.ts
@@ -1,8 +1,22 @@
 import { ParkingLot } from './ParkingLot'
 
-test('should calculate value of ticket to a car that was parked for 3 hours', () => {
-  const parkingLot = new ParkingLot()
+test('should calculate value of ticket to a car that was parked for 3 hours beach', () => {
+  const parkingLot = new ParkingLot('beach')
   parkingLot.checkIn('ABC-1234', new Date('2020-10-10T10:00:00'))
   const ticket = parkingLot.checkOut('ABC-1234', new Date('2020-10-10T13:00:00'))
   expect(ticket.amount).toBe(15)
+})
+
+test('should calculate value of ticket to a car that was parked for 2 hours shopping', () => {
+  const parkingLot = new ParkingLot('shopping')
+  parkingLot.checkIn('ABC-1234', new Date('2020-10-10T12:00:00'))
+  const ticket = parkingLot.checkOut('ABC-1234', new Date('2020-10-10T14:00:00'))
+  expect(ticket.amount).toBe(0)
+})
+
+test('should calculate value of ticket to a car that was parked for 10 hours airport', () => {
+  const parkingLot = new ParkingLot('airport')
+  parkingLot.checkIn('ABC-1234', new Date('2020-10-10T10:00:00'))
+  const ticket = parkingLot.checkOut('ABC-1234', new Date('2020-10-10T20:00:00'))
+  expect(ticket.amount).toBe(31)
 })
--- 2-solid-parking-lot/src/ParkingLot.ts
+++ 2-solid-parking-lot/src/ParkingLot.ts
@@ -1,8 +1,10 @@
 export class ParkingLot {
   tickets: { plate: string; checkInDate: Date }[]
+  location: string
 
-  constructor() {
+  constructor(location: string) {
     this.tickets = []
+    this.location = location
   }
 
   checkIn(plate: string, checkInDate: Date) {
@@ -13,7 +15,24 @@ export class ParkingLot {
     const ticket = this.tickets.find((ticket) => ticket.plate === plate)
     if (!ticket) throw new Error('Ticket not found')
     const period = (checkoutDate.getTime() - ticket.checkInDate.getTime()) / (1000 * 60 * 60)
-    const amount = period * 5
+    let amount = 0
+    if (this.location === 'beach') {
+      amount = period * 5
+    }
+    if (this.location === 'shopping') {
+      if (ticket.checkInDate.getHours() >= 12 && ticket.checkInDate.getHours() <= 14) {
+        amount = 0
+      } else {
+        amount = period * 3
+      }
+    }
+    if (this.location === 'airport') {
+      amount = 10
+      const remainingHours = period - 3
+      if (remainingHours > 0) {
+        amount += remainingHours * 3
+      }
+    }
     return {
       amount,
     }
