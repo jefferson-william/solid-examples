d5655c0ce21adb3b1484b4d45fb885cf689c99be
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2021-09-04 15:10:04 -0300

    refactor(Beach): calculate free period in Beach not is zero, but yes throw error not implemented

--- 2-solid-parking-lot/src/Beach.ts
+++ 2-solid-parking-lot/src/Beach.ts
@@ -1,13 +1,11 @@
 import { ParkingLocation } from './ParkingLocation'
 
 export class Beach extends ParkingLocation {
-  private freeHours = 0
-
   constructor(name: string, hours: number) {
     super('Beach', name, hours)
   }
 
   calculateFreePeriod(): number {
-    return this.freeHours
+    throw new Error('Not implemented')
   }
 }
